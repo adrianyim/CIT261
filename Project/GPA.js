b1cd7a6588799038249a21c2a51dd88a971228b2
adrianyim <adriany93@gmail.com>
2018-10-26 14:56:27 -0600

    10/26 updated

--- Project/GPA.js
+++ Project/GPA.js
@@ -1,74 +1,46 @@
-// Global varbies
+// Global arrays
 var grades = ["A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F"];
 var credits = [4, 3.7, 3.4, 3, 2.7, 2.4, 2, 1.7, 1.4, 1, 0.7, 0];
-var inGrades = new Array(10);
-var inCredits = new Array(10);
-var className = new Array(10);
+var inGrades = [];
+var inCredits = [];
+var className = [];
 
 // A valid function
 function valid() {
     retrieve();
-    
+
+    var num = className.length;
+    console.log(num);
+
     // Check empty or not
     var error = false;
-    for ( var i=0; i<document.getElementById("semester_tbody").rows.length - 1; i++) {
-        if (className[i] == "") {
+    for ( var i=0; i<className.length; i++) {
+        if (className[i] === "") {
             alert("Input Error: Class Name " + eval(i+1) + " is missing!");
             //document.getElementById('error').innerHTML = "Class name is missing";
             error = true;
         }
         
-        if (inGrades[i] == "") {
+        if (inGrades[i] === "") {
             alert("Input Error: Grade of Course " + eval(i+1) + " is missing!");
             //document.getElementById('error').innerHTML = " Grade is missing";
             error = true;
         }
             
-        if (inCredits[i] == ""){
+        if (inCredits[i] === "") {
             alert("Input Error: Credit of Course " + eval(i+1) + " is missing!");
             //document.getElementById('error').innerHTML = " Credit is missing";
             error = true;
         }
 
-        if (error == true) {
+        if (error === true) {
             break;
         }
     }
 
     // If there are no empty, then run the gpaCalc function
-    if (error == false) {
+    if (error === false) {
         gpaCalc();
-    }
-}
-
-// Display instrustion function
-function disInstruction() {
-    document.getElementById("instruct").onclick = function(){
-        document.getElementById("display").innerHTML = "1. Enter your courses.<br/>2. Enter your perdicted or current grades.<br/>3. Enter your course credits.<br/>4. Click CALCULATE button.<br/>5. You can see your result.<br/>6. You may reset and repeat step (1 - 5).";
-    }
-}
-
-// Display about function
-function disAbout() {
-    document.getElementById("about").onclick = function(){
-        document.getElementById("display").innerHTML = "This is a simple GPA Calculator. The goal of this calculator is to predict and set your semester GPA by providing your class' grades and credits. <br/>This calculator provides you to have a short amount time to find out how many GPA you need. You will feel confident with the accurate information by using the GPA calculator.";
-    }
-}
-
-// Clear inputs and result
-function getClear() {
-    document.getElementById("result").innerHTML = " ";
-}
-
-// Get input error**************************************
-function inputError() {
-    var input = document.getElementsByName("CR1").value;
-    if(0 <= input <= 5) {
-        return true;
-    }
-    else {
-        alert("ERROR: The first row of the credit is incorrect format!");
-        return false;
     }
 }
 
@@ -102,13 +74,45 @@ function gpaCalc() {
     }
     
     //It prevents a divide by zero error
-    if (totalCredits == 0) {
+    if (totalCredits === 0) {
         alert("Input Error: Credit cannot be zero!");
         return 0;
     }
     
+    // GPA formula
     gpa = Math.round(totalGrades/totalCredits*100)/100;
     report(gpa);
+}
+
+// Display instrustion function
+function disInstruction() {
+    document.getElementById("instruct").onclick = function(){
+        document.getElementById("display").innerHTML = "1. Enter your courses.<br/>2. Enter your perdicted or current grades.<br/>3. Enter your course credits.<br/>4. Click CALCULATE button.<br/>5. You can see your result.<br/>6. You may reset and repeat step (1 - 5).";
+    }
+}
+
+// Display about function
+function disAbout() {
+    document.getElementById("about").onclick = function(){
+        document.getElementById("display").innerHTML = "This is a simple GPA Calculator. The goal of this calculator is to predict and set your semester GPA by providing your class' grades and credits. <br/>This calculator provides you to have a short amount time to find out how many GPA you need. You will feel confident with the accurate information by using the GPA calculator.";
+    }
+}
+
+// Clear inputs and result
+function getClear() {
+    document.getElementById("result").innerHTML = " ";
+}
+
+// Get input error**************************************
+function inputError() {
+    var input = document.getElementsByName("CR1").value;
+    if(0 <= input <= 5) {
+        return true;
+    }
+    else {
+        alert("ERROR: The first row of the credit is incorrect format!");
+        return false;
+    }
 }
 
 // Retrieve inputs function
@@ -136,7 +140,7 @@ function report(gpa) {
     document.getElementById("result").innerHTML = report;
     
     for (var i=0; i<document.getElementById("semester_tbody").rows.length - 1; i++) {
-        if (inGrades[i] == "none") {
+        if (inGrades[i] === "") {
             break;
         }
         document.getElementById("result").innerHTML += "Class: " + className[i] + "&nbsp&nbsp&nbsp&nbsp Grade: " + inGrades[i] + "&nbsp&nbsp&nbsp&nbsp Credit: " + inCredits[i] + "<br/>";
@@ -147,7 +151,7 @@ function report(gpa) {
 
 // Add classes function
 function addRow() {
-    var table = document.getElementById("semester_tbody");
+    var table = document.getElementById('semester_tbody');
     var row = table.insertRow(document.getElementById("semester_tbody").rows.length - 1);
 
     row.insertCell(0).innerHTML = (document.getElementById('semester_tbody').rows.length - 1) + ". <input type='text' name='n" + (document.getElementById("semester_tbody").rows.length - 1) + "' size='10'>";
@@ -157,4 +161,9 @@ function addRow() {
     // cell1.innerHTML = (document.getElementById('semester_tbody').rows.length - 1) + ". <input type='text' name='n" + (document.getElementById("semester_tbody").rows.length - 1) + "' size='10'>";
     // cell2.innerHTML = "<select id='GR" + (document.getElementById("semester_tbody").rows.length - 1) + "'><option value='none'>—</option><option value='A'>A</option><option value='A-'>A-</option><option value='B+'>B+</option><option value='B'>B</option><option value='B-'>B-</option><option value='C+'>C+</option><option value='C'>C</option><option value='C-'>C-</option><option value='D+'>D+</option><option value='D'>D</option><option value='D-'>D-</option><option value='F'>F</option></select>";
     // cell3.innerHTML = "<input type='text' size='1' name='CR" + (document.getElementById("semester_tbody").rows.length - 1) + "' ALIGN=TOP maxlength='1'>";
+}
+
+// Remove classes function
+function removeRow() {
+    document.getElementById('semester_tbody').deleteRow(document.getElementById("semester_tbody").rows.length - 1);
 }
